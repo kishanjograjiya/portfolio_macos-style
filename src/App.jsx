d6715d4e35b205e8f1d84d2f8328b3f81aee8435
Kishan Jograjiya <kishanjograjiya.dev@gmail.com>
2025-12-29 23:00:34 +0530

    feat: add Photos window component and associated SVG icons
    - Introduced a new Photos component to display a gallery and sidebar with photo links.
    - Added new SVG icons for Library, Memories, People, and Favorites.
    - Updated constants to include photo links and gallery images.
    - Enhanced CSS for the Photos component layout and styling.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
   Text,
   Image,
   Contact,
+  Photos,
 } from "#windows";
 
 gsap.registerPlugin(Draggable);
@@ -27,6 +28,7 @@ const App = () => {
       <Image />
       <Contact />
       <Home />
+      <Photos />
     </main>
   );
 };
